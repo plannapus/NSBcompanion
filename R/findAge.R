bb3d2549542f7805751769b2a86cd1e7a4023fb8
johan.renaudie <johan.renaudie@mfn-berlin.de>
2025-05-21 18:15:24 +0200

    corrected a bunch f small mistakes

--- R/findAge.R
+++ R/findAge.R
@@ -1,9 +1,9 @@
 findAge <-
 function(conn, hole_id, depth_mbsf){
   site_hole <- dbGetQuery(conn,sprintf("SELECT site_hole FROM neptune_hole_summary WHERE hole_id='%s';",hole_id))
-  if(!length(site_hole)) stop("hole_id not found.")
+  if(!nrow(site_hole)) stop("hole_id not found.")
   revision_no <- dbGetQuery(conn,sprintf("SELECT revision_no FROM neptune_age_model_history WHERE site_hole='%s' AND current_flag='Y';", site_hole))
-  if(length(revision_no)>0){
+  if(nrow(revision_no)>0){
     nam <- dbGetQuery(conn,sprintf("SELECT depth_mbsf, age_ma FROM neptune_age_model WHERE site_hole='%s' AND revision_no=%i;", site_hole, revision_no[1,1]))
     nam <- nam[order(nam$depth_mbsf, nam$age_ma),]
     if(nrow(nam)>0){
